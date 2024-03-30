fd289a4f057ca6a2b7d1058a764ce85234b45a66
pelagiamelati2004 <pelagiamelati2004@gmail.com>
2024-03-30 16:35:06 +0700

    Tugas 1

---
+++ application/Tugas1.php
@@ -0,0 +1,11 @@
+<?php
+class Tugas1 extends CI-Controller
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo"nama saya pelagia melati
+        saya berasal dari gunung tamang,kabupaten kubu raya
+        saya suka mendengarkan musik dan mendengarkan film";
+    }
+}
